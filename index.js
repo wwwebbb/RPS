1940f5710dfeea001a9400d036e1b56102a9b9d9
Miles <milesweldo@gmail.com>
2022-12-28 14:41:20 -0500

    logic refactored because why not

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var winCondition = '';
 var newPoint = '';
 var userPoint = 0;
 var computerPoint = 0;
@@ -15,62 +14,78 @@ $('button').click(function () {
     .setAttribute('style', 'visibility: visible');
   var userChoice = this.id;
   var randomDrawString = randomDraw();
-  //User Loses Conditions
-  if (
-    (userChoice === 'Rock' && randomDrawString === 'Paper') ||
-    (userChoice === 'Paper' && randomDrawString === 'Scissors') ||
-    (userChoice === 'Scissors' && randomDrawString === 'Rock')
-  ) {
-    winCondition = `${randomDrawString} beats ${userChoice.toLowerCase()} :( Choose again`;
-    newPoint = 'Computer Point';
-    $('.game-status-text').css('color', 'red');
 
-    //User Wins Conditions
-  } else if (!(userChoice === randomDrawString)) {
-    winCondition = `${userChoice} beats ${randomDrawString.toLowerCase()}! Choose again`;
-    newPoint = 'User Point';
-    $('.game-status-text').css('color', 'green');
+  // maps each possible choice to the choice that beats it
+  const outcomes = {
+    Rock: 'Scissors',
+    Paper: 'Rock',
+    Scissors: 'Paper',
+  };
 
-    //Draw Condition
-  } else {
-    winCondition = "It's a draw :O Choose again";
-    newPoint = '';
-    $('.game-status-text').css('color', 'black');
-  }
+  // win condition logic based on 'outcomes' object
+  const result =
+    userChoice === randomDrawString
+      ? "It's a draw :O Choose again"
+      : outcomes[userChoice] === randomDrawString
+      ? `${userChoice} beats ${randomDrawString.toLowerCase()}! Choose again`
+      : `${randomDrawString} beats ${userChoice.toLowerCase()} :( Choose again`;
+
+  // adds point to either user or computer
+  const newPoint =
+    userChoice === randomDrawString
+      ? ''
+      : outcomes[userChoice] === randomDrawString
+      ? 'User Point'
+      : 'Computer Point';
+
+  //updates win condition text color
+  $('.game-status-text').css(
+    'color',
+    userChoice === randomDrawString
+      ? 'black'
+      : outcomes[userChoice] === randomDrawString
+      ? 'green'
+      : 'red'
+  );
 
   $('.userChoice').text(`You chose ${userChoice}...`);
   $('.computerChoice').text(`The computer chose ${randomDrawString}...`);
-  $('.game-status-text').text(winCondition);
-  var winConditionSlice = winCondition.slice(0, winCondition.length - 12);
+  $('.game-status-text').text(result);
+  var winConditionSlice = result.slice(0, result.length - 12);
+  tallyPoints(newPoint, winConditionSlice);
+});
 
+function tallyPoints(newPoint, winConditionSlice) {
   //Talleys the points and sends to endGameResultScreen function
   if (newPoint === 'User Point') {
     $('.userResultText').text($('.userResultText').text().concat('I'));
     userPoint++;
-    if (userPoint === 2) {
-      $('.game-status-text').text(
-        `${winConditionSlice} You won the entire game!`
-      );
-      endGameResultScreen();
-    }
   } else if (newPoint === 'Computer Point') {
     $('.computerResultText').text($('.computerResultText').text().concat('I'));
     computerPoint++;
-    if (computerPoint === 2) {
-      $('.game-status-text').text(
-        `${winConditionSlice} The computer won the entire game :(`
-      );
-      endGameResultScreen();
-    }
   }
-});
+
+  if (userPoint === 2) {
+    $('.game-status-text').text(
+      `${winConditionSlice} You won the entire game!`
+    );
+    endGameResultScreen();
+  } else if (computerPoint === 2) {
+    $('.game-status-text').text(
+      `${winConditionSlice} The computer won the entire game :(`
+    );
+    endGameResultScreen();
+  }
+}
 
 //End Game button properties
 function endGameResultScreen() {
   // End Screen animation trigger
   $('.game-status-text').addClass('end-animation');
+
   //Disables RPS buttons
   $('.gameButton').attr('disabled', true);
+
   //adds "New Game" button to results section to demonstrate DOM manipulation, but a "visibility: hidden;" method could've been used in the html flow
   const talleyParent = document.getElementById('talleyParent');
   const crtChild = document.getElementById('crtChild');
@@ -93,7 +108,6 @@ function endGameResultScreen() {
       .text('Choose rock, paper, or scissors to start playing!')
       .css('color', 'black');
     $('.userChoice, .computerChoice').text('');
-    winCondition = '';
     newPoint = 0;
     userPoint = 0;
     computerPoint = 0;
